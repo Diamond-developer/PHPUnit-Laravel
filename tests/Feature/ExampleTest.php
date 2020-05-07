13fd644e14736e67bdd9f532223dddd5a7923e8a
diamond <cuonggvu97@gmail.com>
2020-05-07 09:28:10 +0700

    upload epsi 15

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -32,5 +32,56 @@ class ExampleTest extends TestCase
         $response->assertStatus(200);
 
     }
+
+    //epsi 14
+    /**@Test*/
+    function it_normalizes_a_string_for_the_cache_key()
+    {
+        $cache = $this->prophesize(RussianCache::class);
+        $directive = new BladeDirective($cache->reveal());
+
+
+        $cache->has('cache-key')->shouldBeCalled();
+
+
+        $directive->setUp('cache-key');
+
+    }
+
+    /**@Test*/
+    function it_normalizes_a_cacheable_model_for_the_cache_key()
+    {
+        $cache = $this->prophesize(RussianCache::class);
+        $directive = new BladeDirective($cache->reveal());
+
+
+        $cache->has('stub-cache-key')->shouldBeCalled();
+
+
+        $directive->setUp(new ModelStub);
+
+    }
+
+
+    /**@Test*/
+    function it_normalizes_an_array_for_the_cache_key()
+    {
+        $cache = $this->prophesize(RussianCache::class);
+        $directive = new BladeDirective($cache->reveal());
+
+
+        $cache->has(md5('foobar'))->shouldBeCalled();
+
+
+        $directive->setUp(['foo', 'bar']);
+    }
+}
+
+class ModelStub
+{
+    public function getCacheKey()
+    {
+        return 'stub-cache-key';
+    }
 }
 
